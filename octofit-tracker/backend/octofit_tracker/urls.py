36092beb313d3765d81db89671a1a9d8103542ac
Roberto Cuccurese <roberto.cuccurese@eng.it>
2026-04-29 17:10:18 +0000

    fix: redirect root / su /api per Django backend

--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 from rest_framework.routers import DefaultRouter
 from . import views
 import os
@@ -39,6 +40,7 @@ def api_info(request):
     })
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/api/', permanent=False)),
     path('admin/', admin.site.urls),
     path('api/', api_info, name='api-info'),
     path('api/', include(router.urls)),
